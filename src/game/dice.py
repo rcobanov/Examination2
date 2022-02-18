bf1b05393e34502bf14eb94504a81b2c879115e1
robin <robin.cobanov@hotmail.com>
2022-02-18 11:35:34 +0100

    Modifying dice class

--- src/game/dice.py
+++ src/game/dice.py
@@ -1,8 +1,11 @@
 import random
-# test ssh
-
 
 class Dice():
-
+    faces = 6
+    
     def __init__(self):
         random.seed()
+
+    def roll_dice(self):
+        roll = random.randint(1, self.faces)
+        return roll

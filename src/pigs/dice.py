e274b6e31fedd0c6343e09c914902f10d4dfe5de
robin <robin.cobanov@hotmail.com>
2022-02-22 22:50:19 +0100

    Added functionality when dice rolls 1

--- src/pigs/dice.py
+++ src/pigs/dice.py
@@ -2,10 +2,12 @@ import random
 
 class Dice():
     faces = 6
+    thisRoll = 0
     
     def __init__(self):
         random.seed()
 
     def roll(self):
         roll = random.randint(1, self.faces)
+        self.thisRoll = roll
         return roll

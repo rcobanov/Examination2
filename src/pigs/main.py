8a83707f9dac776a8f2943a4586f184f702603d2
fwett <fwetterlov@gmail.com>
2022-02-22 14:20:15 +0100

    Deletat game-mappen

--- src/pigs/main.py
+++ src/pigs/main.py
@@ -3,6 +3,8 @@ import dice
 from pigs.game import Game
 import player
 # välkommen till råttboet chiefen!
+# Tror inte det är lönt att du utgår från mina import, har nog kladdat bort allt som var värdefullt där,
+# Samma när jag kallar på startPigs
 
 
 def main():

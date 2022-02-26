c4b65fc22443fd741aa09b84a49d5d60ea6e4a20
fwett <fwetterlov@gmail.com>
2022-02-26 12:12:47 +0100

    Added the ocd diary in main

--- src/pigs/dice.py
+++ src/pigs/dice.py
@@ -12,13 +12,3 @@ class Dice():
         roll = random.randint(1, self.faces)
         self.this_roll = roll
         return roll
-
-    def rollMultipleTimes(self, times_to_roll):
-        total_rolls = 0
-        for x in range(times_to_roll):
-            roll = random.randint(1, self.faces)
-            total_rolls += roll
-            if roll == 1:
-                return 1
-                break
-        return total_rolls
